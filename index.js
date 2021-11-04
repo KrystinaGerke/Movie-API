db235248d475569af11b71e3b3009bcc47a861fd
KrystinaGerke <83786005+KrystinaGerke@users.noreply.github.com>
2021-11-04 14:51:00 -0400

    updated endpoints with errors for help

--- index.js
+++ index.js
@@ -1,96 +1,107 @@
+const mongoose = require('mongoose');
+const Models = require('./models.js');
+const bodyParser = require('body-parser');
+
+const Movies = Models.Movie;
+const Users = Models.User;
+const Genres = Models.Genre;
+const Directors = Models.Directors;
+
+mongoose.connect('mongodb://localhost:27017/myFlix', {
+  useNewUrlParser: true, useUnifiedTopology: true
+});
+
 const express = require('express');
 morgan = require('morgan');
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-let topMovies = [
-  {
-    title: 'Dune',
-    director: 'Frank Herbert'
-  },
-  {
-    title: 'Stargate',
-    director: 'Ronald Emmerich'
-  },
-  {
-    title: 'Underworld',
-    director: 'Len Wiseman'
-  },
-  {
-    title: 'The Neverending Story',
-    director: 'Wolfgang Petersen'
-  },
-  {
-    title: 'The Princess Bride',
-    director: 'Rob Reiner'
-  },
-  {
-    title: 'The Goonies',
-    director: 'Joe Dante'
-  },
-  {
-    title: 'Van Helsing',
-    director: 'Stephen Sommers'
-  },
-  {
-    title: 'Serenity',
-    director: 'Joss Whedon'
-  },
-  {
-    title: 'The Fifth Element',
-    director: 'Luc Besson'
-  },
-  {
-    title: 'Star Trek Into Darkness',
-    director: 'JJ Abrams'
-  },
-];
 
 app.use(morgan('common'));
 
-// GET requests
+//READ
 app.get('/', (req, res) => {
   res.send('Welcome to my movie club!');
 });
 
+//READ
 app.use(express.static('public'));
 app.get('/documentation', (req, res) => {
 	res.sendFile('public/documentation.html', { root: __dirname });
 });
 
+//READ
 app.get('/movies', (req, res) => {
-  res.json(topMovies);
+  res.status(200).json(Movie);
 });
 
+//READ
 app.get('/movies/:movieTitle', (req, res) => {
-  let movie = topMovies.find(m => m.title === req.params.movieTitle)
-  res.json(movie)
+  const {movieTitle} = req.params;
+  const movie = movies.find(movie => movie.Title === title );
+
+  if (movie) {
+    res.status(200).json(movie);
+  } else {
+    res.status(400).send('This movie is not in our database')
+  }
 }); 
 
+//READ
 app.get('/genres/:genre', (req, res) => {
-  res.send('Successful GET request of all major genres in the movie field.');
+  const {genreName} = req.params;
+  const genre = movies.find(movie => movie.Genre.Name === genreName).Genre;
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send('This genre is not in our database')
+  }
 }); 
 
+//READ
 app.get('/directors/:directorName', (req, res) => {
-  res.send('Successful GET request of directors information.');
+  const {directorName} = req.params;
+  const directors = movies.find(movie => movie.Director.Name === directorName).Director;
+
+  if (directors) {
+    res.status(200).json(directors);
+  } else {
+    res.status(400).send('This director is not in our database')
+  }
 }); 
 
-app.post('/register', (req, res) => {
-  res.send('Successful POST to the server a new users information.');
+//CREATE
+app.post('/users', (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser)
+  } else {
+    res.status(400).send('All users need a Username')
+  }
 }); 
 
+//UPDATE
 app.put('/users/:ID/:infoToUpdate/:newValue', (req, res) => {
   res.send('Successful PUT to update users username information.');
 }); 
 
+//CREATE
 app.post('/users/:ID/favorites/:newFavorite', (req, res) => {
   res.send('Successful POST to the server for a new favorite movie on a specific users profile.');
 }); 
 
+//DELETE
 app.delete('/users/:id/unregister', (req, res) => {
   res.send('Successful DELETE to the server that removes a user from the database.');
 }); 
 
+//DELETE
 app.delete('/users/:ID/favorites/:deleteFavorite', (req, res) => {
   res.send('Successful DELETE of a movie from an individual users favorite list on the database.');
 }); 
